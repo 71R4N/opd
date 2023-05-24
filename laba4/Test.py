f7ededbe2c05a268f150a45ada2ad2b9eaba43fc
Ilya Baburov <125269037+71R4N@users.noreply.github.com>
2023-05-24 11:36:07 +0600

    Update Test.py

--- laba4/Test.py
+++ laba4/Test.py
@@ -13,25 +13,19 @@ class TestIndex(unittest.TestCase):
     def test_index_post_with_file(self):
         with open('test.txt', 'w') as f:
             f.write('hello world world')
-
         with open('test.txt', 'rb') as f:
             response = self.app.post('/', data={'file': (f, 'test.txt')})
 
         result_html = response.data.decode()
-
-        # check if most_frequent_word is in the rendered HTML
         self.assertIn('<h1>Самое частое слово: world</h1>', result_html)
 
     def test_post_empty_file(self):
         with open('test.txt', 'w') as f:
             f.write('')
-
         with open('test.txt', 'rb') as f:
             response = self.app.post('/', data={'file': (f, 'test.txt')})
 
         result_html = response.data.decode()
-
-        # check if most_frequent_word is in the rendered HTML
         self.assertIn('<h1>Файл пуст</h1>', result_html)
 
 
